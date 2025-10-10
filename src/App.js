1fa14da8c3ddf994354e908cc9671f51187f0ff2
bela0508 <bela0508@algonquinlive.com>
2025-10-10 03:25:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    
-    <GluestackUIProvider mode="dark">
-      <View>
+    <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-    </GluestackUIProvider>
-  
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
